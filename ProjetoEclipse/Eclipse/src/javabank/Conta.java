4308539a415a34ef9d1b771390a226da145dc5bf
Thiago Erick Lauriano Cruz Gomes <Thiagolauriano0301@gmail.com>
2025-12-01 14:19:31 -0300

    Adicionado a classe sistema

--- ProjetoEclipse/Eclipse/src/javabank/Conta.java
+++ ProjetoEclipse/Eclipse/src/javabank/Conta.java
@@ -1,5 +1,22 @@
 package javabank;
 
 public class Conta {
+	public int numero;
+	private double saldo;
+	
+	public int getnumero() {
+		return numero;
+	}
+	public double getsaldo() {
+		return saldo;
+	}
+	public void setnumero(int numero) {
+		this.numero = numero;
+	}
+	public void setsaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
 
 }
+
---
+++ ProjetoEclipse/Eclipse/src/javabank/Sistema.java
@@ -0,0 +1,11 @@
+package javabank;
+
+public class Sistema {
+	private Usuario usuariologado;
+	public Usuario getusuariologado() {
+		return usuariologado;
+	}
+	public void setusuariologado(Usuario usuariologado) {
+		this.usuariologado = usuariologado;
+	}
+}
